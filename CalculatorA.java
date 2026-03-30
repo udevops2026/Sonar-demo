080f2bf30902b1be48602823275433d642523b27
udevops2026 <udevops2026@gmail.com>
2026-03-30 15:49:47 +0530

    testing

--- CalculatorA.java
+++ CalculatorA.java
@@ -20,24 +20,4 @@ public class CalculatorA {
 
         System.out.println(sum);
     }
-
-    public void method2() {
-        int sum = 0;
-
-        // SAME BLOCK AGAIN (duplication)
-        for (int i = 0; i < 100; i++) {
-            sum += i;
-            sum += i * 2;
-            sum += i * 3;
-            sum += i * 4;
-            sum += i * 5;
-            sum += i * 6;
-            sum += i * 7;
-            sum += i * 8;
-            sum += i * 9;
-            sum += i * 10;
-        }
-
-        System.out.println(sum);
-    }
-}
+   }
